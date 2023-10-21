12764e09b1a496bd873e5b358a9c44ed0c90d88a
Maxim Fissyuk <mfissyuk20@gmail.com>
2023-10-21 22:23:37 +0200

    add: selectAll, removeAll, sidebar

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Sidebar.module.scss'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div>All</div>
+      <div></div>
+      <div>all</div>
+    </div>
+  )
+}
+
+export default Sidebar
